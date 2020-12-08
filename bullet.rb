80bc416cec8d0fa4abb0f04cda8466020a8c85ff
taratrou <kapibara4003@dune.ocn.ne.jp>
2020-12-08 07:34:28 +0000

    銃弾

---
+++ bullet.rb
@@ -0,0 +1,18 @@
+require 'dxopal'
+include DXOpal
+Image.register(:Tama,'images/player.png')
+
+class Bullet < Sprite
+  
+  def initialize(x,y)
+    @now_x=x
+    @now_y=y
+    @image = Image[:Tama]
+    super(@now_x,@now_y,@image)
+  end
+  
+  def update
+      @y -= 10
+  end
+  
+end  
---
+++ enemy.rb
@@ -0,0 +1,31 @@
+require 'dxopal'
+include DXOpal
+
+Image.register(:apple, 'images/apple.png')
+# アイテムを表すクラスを追加
+class Enemy < Sprite
+  attr_reader :x, :y
+  def initialize
+    image = Image[:apple]
+    @x = 200  # x座標をランダムに決める
+    @y = 400
+    @vec = true #right=>True
+    super(@x, @y, image)
+  end
+
+  def update
+    if @vec == true
+      @x += 4
+      if @x > 300
+        @vec = false
+      end
+    
+    elsif @vec == false
+      @x -= 4
+      if @x < 100
+        @vec = true
+      end
+      
+    end
+  end
+end
